2d845e5402a8173e80f2ea8dd86b671503f3cf5b
Bruno Quaresma <bruno_nonato_quaresma@hotmail.com>
2021-09-11 11:03:21 -0300

    Add meta

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,19 @@ import type { NextPage } from "next";
 import React from "react";
 import { Game } from "../game/components/Game";
 import Footer from "../layout/components/Footer";
+import Head from "next/head";
 
 const Home: NextPage = () => {
   return (
     <>
+      <Head>
+        <title>Memory game</title>
+        <meta
+          name="description"
+          content="A free and open-source memory game built using XState and Typescript"
+        />
+      </Head>
+
       <Game />
       <Footer />
     </>
